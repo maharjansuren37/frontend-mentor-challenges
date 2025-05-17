66f18fe67251de16d51d3291f004fa64aa7fda1b
maharjansuren37 <maharjansuren37@gmail.com>
2025-05-17 21:56:38 +0930

    styles: card and section

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// TOP NAVIGATION
 // Toggle Menu
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav-list');
+const navLink = document.querySelectorAll('.nav-link');
+const surpriseBtn = document.querySelector('.btn-surprise');
+const challengeCounter = document.querySelector('.challenge-counter');
 
 navToggle.addEventListener('click', () => {
   const dataVisible = nav.getAttribute('data-visible');
@@ -13,3 +17,16 @@ navToggle.addEventListener('click', () => {
     navToggle.setAttribute('aria-expanded', false);
   }
 });
+
+surpriseBtn.addEventListener('click', () => challengeCounter.classList.toggle('hidden'));
+
+// Active Nav Links
+// navLink.forEach(n => {
+//   n.addEventListener('click', () => {
+//     if (n.textContent === 'Challenges') {
+//       n.classList.toggle('active');
+//     } else if (n.textContent === 'About') {
+//       n.classList.toggle('active');
+//     }
+//   })
+// });
